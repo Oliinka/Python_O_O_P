8575d8572784558bd4ffa9b529b7a4395d9dd8d6
Oliinka <olga.titzenthaler@gmail.com>
2023-11-01 00:48:53 +0100

    knock

--- knock_knock/knock_knock.py
+++ knock_knock/knock_knock.py
@@ -4,9 +4,11 @@ class KnockKnock:
             user_input = input("> ")
             if "Who" in user_input:
                 print("Cow")
+                break
             if "Cow" in user_input:
                 print("Cow don't whoo, cow moo! :-D")
                 break
+            
             
     def decider(self):
         while True:
